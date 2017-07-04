697e4fd47bd096fc157b6b859ea0cf846d21fffe
freed24 <dfreed24@yahoo.com>
2017-07-04 18:37:50 +0800

    添加标题

--- tk_iccs_gui.rb
+++ tk_iccs_gui.rb
@@ -2,8 +2,10 @@
 # @Author: david
 # @Date:   2017-07-04 18:16:06
 # @Last Modified by:   anchen
-# @Last Modified time: 2017-07-04 18:23:27
+# @Last Modified time: 2017-07-04 18:36:34
 require 'tk'
 require 'date'
 
+# 图形界面生成
+root = TkRoot.new { title "ICCS处理"}
 Tk.mainloop
