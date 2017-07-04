189519357c211924db7f975973a9d3bbd3752c0c
freed24 <dfreed24@yahoo.com>
2017-07-04 18:22:31 +0800

    create tk_iccs_gui.rb

---
+++ tk_iccs_gui.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/ruby
+# @Author: david
+# @Date:   2017-07-04 18:16:06
+# @Last Modified by:   anchen
+# @Last Modified time: 2017-07-04 18:21:07
+require 'tk'
+require 'date'
+
+Tk.mainloop
