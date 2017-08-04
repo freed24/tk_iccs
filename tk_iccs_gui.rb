33cb5a0ca53dbfd6417466ba361f75aea97fb7f3
freed24 <dfreed24@yahoo.com>
2017-08-04 18:37:45 +0800

    添加界面类

--- tk_iccs_gui.rb
+++ tk_iccs_gui.rb
@@ -2,19 +2,47 @@
 # @Author: david
 # @Date:   2017-07-04 18:16:06
 # @Last Modified by:   anchen
-# @Last Modified time: 2017-07-04 19:01:56
+# @Last Modified time: 2017-08-04 18:36:26
 require 'tk'
 require 'date'
 
-# 图形界面生成
-root = TkRoot.new { title "ICCS处理"}
-frame_area_code = TkFrame.new(root){pack}
-frame_quarter = TkFrame.new(root){pack}
-frame_20 = TkFrame.new(root){pack}
-frame_30 = TkFrame.new(root){pack}
-frame_40 = TkFrame.new(root){pack}
+class TkIccsGui
+  def iccs_run
 
-TkLabel.new(frame_area_code){text '机构代码'
-  pack(side:'left', padx:5, pady:10)}
+  end
 
+  def initialize
+    # 获取当前时间
+    t = Time.now
+    # 当前月份减一
+    last_month = Date.new(t.year, t.month, t.day) << 1
+    option = TkVariable.new('month')
+    @year_month = Array.new
+
+    # 业务数据处理
+
+
+    # 图形界面生成
+    root = TkRoot.new { title "ICCS处理"}
+    frame_area_code = TkFrame.new(root){pack}
+    frame_quarter = TkFrame.new(root){pack}
+    frame_20 = TkFrame.new(root){pack}
+    frame_30 = TkFrame.new(root){pack}
+    frame_40 = TkFrame.new(root){pack}
+    TkLabel.new(frame_area_code){text '机构代码'
+      pack(side:'left', padx:5, pady:10)}
+    area_code = TkVariable.new
+    area_code.value = '610100'
+    @entry_code = TkEntry.new(frame_area_code, textvariable:area_code){
+      width 6
+      pack(side:'left', padx:5, pady:10)
+    }
+  end
+end
+
+
+
+
+
+TkIccsGui.new
 Tk.mainloop
